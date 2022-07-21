c6bff91510bd5e551aa133d57c05294fe4112d96
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-20 19:34:58 -0700

    coded a method to print the data of the nodes of a tree diagonal wise

--- src/DiagnolTraversalBinaryTree.java
+++ src/DiagnolTraversalBinaryTree.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class DiagnolTraversalBinaryTree {
 
     class Node{
@@ -9,6 +14,32 @@ public class DiagnolTraversalBinaryTree {
             this.left = left;
             this.right = right;
         }
+    }
+    static List<Integer> diagonalTraversalBinaryTree(Node root){
+        ArrayList<Integer> ans = new ArrayList<>();
+        if(root == null) return ans;
+
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            Node current = q.remove();
+            while(current != null){
+
+                if(current.left != null){
+                    q.add(current.left);
+                }
+
+                current = current.right;
+                ans.add(current.data);
+            }
+        }return ans;
+
+
+
+
+
+
+
     }
     public static void main(String[] args) {
 
