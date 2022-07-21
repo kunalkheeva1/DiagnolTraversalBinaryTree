2a44510993dae4cbf5ca8581dae7d5d80fb721d2
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-20 19:28:59 -0700

    coded a node class to represent the node data, its left and its right

--- src/DiagnolTraversalBinaryTree.java
+++ src/DiagnolTraversalBinaryTree.java
@@ -1,6 +1,15 @@
 public class DiagnolTraversalBinaryTree {
 
-    c
+    class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data, Node left, Node right){
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
     public static void main(String[] args) {
 
     }
