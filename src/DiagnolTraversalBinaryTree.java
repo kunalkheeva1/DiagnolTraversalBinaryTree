5284792cf1d306552e86463e55780b9268153562
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-20 19:43:16 -0700

    annoted for the better understanding, while current! = null is confusing so read the whole thing

--- src/DiagnolTraversalBinaryTree.java
+++ src/DiagnolTraversalBinaryTree.java
@@ -5,6 +5,7 @@ import java.util.Queue;
 
 public class DiagnolTraversalBinaryTree {
 
+    //node
     class Node{
         int data;
         Node left;
@@ -15,21 +16,27 @@ public class DiagnolTraversalBinaryTree {
             this.right = right;
         }
     }
+    //list of the diagonal elements data
     static List<Integer> diagonalTraversalBinaryTree(Node root){
         ArrayList<Integer> ans = new ArrayList<>();
         if(root == null) return ans;
 
         Queue<Node> q = new LinkedList<>();
         q.add(root);
+        // traverse the queue and take off the node
         while(!q.isEmpty()){
             Node current = q.remove();
+            // pointer continues until it reaches the end on the right side
             while(current != null){
 
+
+                // add the left node in the queue(if any) and then repeat the process
                 if(current.left != null){
                     q.add(current.left);
                 }
-
+                //move your current to right
                 current = current.right;
+                // add the data of your current in the list to return it
                 ans.add(current.data);
             }
         }return ans;
